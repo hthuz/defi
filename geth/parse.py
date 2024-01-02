869ed7eb597650acf9646813f249b6f98c431921
hthuz <hanggang.20@intl.zju.edu.cn>
2024-01-02 20:40:22 +0800

    Add: initial parse

--- geth/parse.py
+++ geth/parse.py
@@ -2,6 +2,10 @@
 import json
 import re
 
+signatures= {
+    "0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822": "UniSwapv2.swap",
+}
+
 def get_trace():
     with open("./trace2.json") as f:
         data = json.load(f)
@@ -9,15 +13,20 @@ def get_trace():
     txids = [x['txHash'] for x in data['result']]
     txs = [x['result']['structLogs'] for x in data['result']]
     TXID=1;
-    print(txids[TXID])
-    for item in txs[TXID]:
-        # print(item.keys())
-        # break
-        if(re.match(r"^LOG.*$", item['op'])):
-            # Show op LOG* only
-            print(f"pc:{item['pc']},op:{item['op']},depth:{item['depth']},stack:{item['stack']},memory:{item['memory']}")
-    print(txids[TXID])
+    for TXID in range(0,len(txs)):
+        print(txids[TXID])
+        for item in txs[TXID]:
+            if(re.match(r"^LOG.*$", item['op'])):
+                # Show op LOG* only
+                # print(f"pc:{item['pc']},op:{item['op']},depth:{item['depth']},stack:{item['stack']},memory:{item['memory']}")
+                parse_item(item)
+    # print(txids[TXID])
 
+def parse_item(item):
+    signature = item['stack'][-3]
+    if signature in signatures.keys():
+        print(signatures[signature])
+    
 
 
 if __name__ == "__main__":
